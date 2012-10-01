10b1a81f44655274518484f53c2807f334017f8a
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-10-01 17:52:40 -0400

    Runs a random task from the app/tasks folder.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,10 @@
 class TasksController < ApplicationController
+  TaskRoot = "#{Rails.root}/app/tasks"
   def next
-    puts 'NEW'
+    tasks = Dir.glob("#{TaskRoot}/task*.js")
+    size = tasks.length
+    task = tasks[rand(size)]
+    puts task
+    render task
   end
 end
